053f4cd0f69f29833b00ec2f3495f531aabc098e
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-12-09 00:39:20 +0300

    Initial commit

---
+++ src/main/java/by/smirnov/wizardbot/WizardbotApplication.java
@@ -0,0 +1,13 @@
+package by.smirnov.wizardbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardbotApplication.class, args);
+	}
+
+}
