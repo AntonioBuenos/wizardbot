e3eb07fdb3e33b615b5a1c5f0bc590f3c6503186
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-12-09 18:36:51 +0300

    Basic Webhook bot added

---
+++ src/main/java/by/smirnov/wizardbot/config/BotConfig.java
@@ -0,0 +1,35 @@
+package by.smirnov.wizardbot.config;
+
+import by.smirnov.wizardbot.WizardTelegramBot;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "telegrambot")
+public class BotConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+    private DefaultBotOptions.ProxyType proxyType;
+    private String proxyHost;
+    private int proxyPort;
+
+    @Bean
+    public WizardTelegramBot WizardTelegramBot() {
+        DefaultBotOptions options = new DefaultBotOptions();
+
+        WizardTelegramBot mySuperTelegramBot = new WizardTelegramBot(options);
+        mySuperTelegramBot.setBotUserName(botUserName);
+        mySuperTelegramBot.setBotToken(botToken);
+        mySuperTelegramBot.setWebHookPath(webHookPath);
+
+        return mySuperTelegramBot;
+    }
+}
---
+++ src/main/java/by/smirnov/wizardbot/controller/WebHookController.java
@@ -0,0 +1,25 @@
+package by.smirnov.wizardbot.controller;
+
+import by.smirnov.wizardbot.WizardTelegramBot;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@RestController
+public class WebHookController {
+    private final WizardTelegramBot telegramBot;
+
+    public WebHookController(WizardTelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+
+}
